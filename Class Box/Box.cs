e1890efeb50d46cfff12c67ebc25f0d89fb22f24
Mario Slavov <hellsflash@gmail.com>
2017-07-03 22:17:39 +0300

    Add project files.

---
+++ Class Box/Box.cs
@@ -0,0 +1,48 @@
+﻿namespace Class_Box
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            this.length = length;
+            this.width = width;
+            this.height = height;
+        }
+
+        public int Length
+        {
+            get { return this.Length; }
+            set { this.Length = value; }
+        }
+        public int Width
+        {
+            get { return this.Width; }
+            set { this.Width = value; }
+        }
+        public int Height
+        {
+            get { return this.Height; }
+            set { this.Height = value; }
+        }
+
+        public double SurfaceArea(double length, double width, double height)
+        {
+            return 2 * (this.length * this.width) + 2 * (this.length * this.height) + 2 * (this.width * this.height);
+        }
+
+        public double LateralSurfaceArea(double length, double width, double height)
+        {
+            return 2 * (this.length * this.height) + 2 * (this.width * this.height);
+        }
+
+        public double Volume(double length, double width, double height)
+        {
+            return this.length * this.height * this.width;
+        }
+        
+    }
+}
---
+++ Class Box/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Class_Box
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Type boxType = typeof(Box);
+            FieldInfo[] fields = boxType.GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+           
+            var length = double.Parse(Console.ReadLine());
+            var width = double.Parse(Console.ReadLine());
+            var height = double.Parse(Console.ReadLine());
+
+            var box = new Box(length,width,height);
+
+            Console.WriteLine(fields.Count());
+            Console.WriteLine($"Surface Area - {box.SurfaceArea(length, width, height):F2}");
+            Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea(length,width,height):F2}");
+            Console.WriteLine($"Volume - {box.Volume(length,width,height):F2}");
+        }
+    }
+}
