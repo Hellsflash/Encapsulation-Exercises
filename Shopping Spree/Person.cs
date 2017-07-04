f2544d1169d028b817bdd498453d8065f741c785
Mario Slavov <hellsflash@gmail.com>
2017-07-04 11:24:19 +0300

    Solved Shopping Spree

---
+++ Shopping Spree/Person.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shopping_Spree
+{
+    public class Person
+    {
+        private string name;
+        private decimal money;
+        private List<Product> products;
+
+        public Person(string name, decimal money)
+        {
+            this.Name = name;
+            this.Money = money;
+            this.products= new List<Product>();
+        }
+        public string Name
+        {
+            get
+            {
+                { return this.name; }
+            }
+            private set
+            {
+                if (value == string.Empty || string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+                this.name = value;
+            }
+        }
+        public decimal Money
+        {
+            get
+            {
+                return this.money;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+                this.money = value;
+            }
+        }
+
+        public void AddProductToBag(Product product)
+        {
+            this.products.Add(product);
+        }
+
+        public List<Product> SeeBag()
+        {
+            return this.products;
+        }
+    }
+}
---
+++ Shopping Spree/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Shopping_Spree
+{
+    public class Product
+    {
+        private string name;
+        public decimal cost;
+
+        public Product(string name, decimal cost)
+        {
+            this.Name = name;
+            this.Cost = cost;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            private set
+            {
+                if (value== string.Empty||string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+                this.name = value;
+            }
+        }
+
+        public decimal Cost
+        {
+            get { return this.cost; }
+            private set { this.cost = value; }
+        }
+
+    }
+}
